d256f48f1ae4ceafcbd8ab860089d1b3a4a7ed34
mulawave <myngulsocial@gmail.com>
2026-03-28 23:57:34 +0100

    Update digital assets screen

--- lib/features/wallet/screens/digital_assets_screen.dart
+++ lib/features/wallet/screens/digital_assets_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../../../core/theme/app_colors.dart';
 import '../../auth/models/user_model.dart';
